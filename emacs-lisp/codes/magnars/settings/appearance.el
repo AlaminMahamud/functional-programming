92635854a34b98687f90b3a639ccbcf8f579d6de
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-01-26 00:41:35 +0600

    add: appearance.el in magnars
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

---
+++ emacs-lisp/codes/magnars/settings/appearance.el
@@ -0,0 +1,13 @@
+;;; Package --- Summary:
+
+;;; Commentary:
+
+;;; Code:
+
+(setq font-lock-maximum-decoration t
+      color-theme-is-global t
+      truncate-partial-width-windows nil)
+
+
+(provide 'appearance)
+;;; appearance.el ends here
